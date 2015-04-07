5014f97708aa97fe8259ca38afceb2a98c13b696
andig <cpuidle@gmx.de>
2015-04-07 08:31:08 +0200

    Initial commit

---
+++ Bootstraps/Zf2.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace PHPPM\Bootstraps;
+
+use Zend\Mvc\Application;
+
+class Zf2 implements BootstrapInterface
+{
+    /**
+     * @var string
+     */
+    protected $appenv;
+
+    /**
+     * Instantiate the bootstrap, storing the $appenv.
+     *
+     * @param string $appenv
+     */
+    public function __construct($appenv)
+    {
+        $this->appenv = $appenv;
+    }
+
+    /**
+     * Create a Zend Framework MVC application.
+     */
+    public function getApplication()
+    {
+        if ($this->appenv) {
+            $filename = "./config/{$this->appenv}.config.php";
+
+        } else {
+            $filename = "./config/application.config.php";
+        }
+
+        if (!file_exists($filename)) {
+            throw new \RuntimeException("Configuration file {$filename} not found.");
+        }
+
+        $config = require $filename;
+
+        $config['service_manager'] = array(
+            'factories' => array(
+                'ServiceListener' => 'PHPPM\Bootstraps\Zf2\Mvc\Service\ServiceListenerFactory',
+            ),
+        );
+
+        return Application::init($config);
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# PHP-PM Zend Adapter
+
+Zend application framework adapter for PHP-PM. See https://github.com/php-pm/php-pm.
+
+### Setup
+
+  1. Install PHP-PM
+
+        composer require php-pm/php-pm:dev-master
+
+  2. Install Zend Adapter
+
+        composer require php-pm/zend-adapter:dev-master
+
